48b8824092575479258d2f6dc3691fe0800396ae
Кирилл Шашичев <kshashichev@tdintant.all>
2021-01-25 11:00:21 +0600

    fix: исправить название файла json

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ import {
 import findModel from "../scripts/components/finder.js";
 
 (async () => {
-    const response = await fetch("./database/new.json")
+    const response = await fetch("./database/models.json")
     const models = await response.json()
 
     statistics.textContent = `Общее количество моделей Hikvision & HiLook в базе поиска: ${models.length} шт.`;
