db29e39f40c81ab3bb244e72770b1a0c56fc6e16
Kirill Shashichev <67040346+Qtrixnet@users.noreply.github.com>
2021-10-13 23:37:32 +0600

    добавить спиннер

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,33 @@
 import './App.css'
 import Header from '../Header/Header';
 import Main from '../Main/Main';
+import Spinner from 'react-bootstrap/Spinner';
+import { useEffect, useState } from "react";
 
 export default function App() {
+
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(true)
+    }, 3000)
+  }, []);
+
   return (
     <div className="App">
-      <Header />
-      <Main />
+      {
+        loading ?
+          <>
+            <Header />
+            <Main />
+          </>
+          :
+          <div className="loading">
+            <h1 class="loading__title">Загружаемся</h1>
+            <Spinner animation="border" variant="success" />
+          </div>
+      }
     </div>
   )
 }
--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react";
 import './Statistics.css'
+import Table from 'react-bootstrap/Table'
 
 export default function Statistics() {
 
-  const [auth, setAuth] = useState();
+  const [auth, setAuth] = useState(false);
 
   useEffect(() => {
     const result = prompt("Введите пароль для получения доступа к статистике");
@@ -11,6 +12,35 @@ export default function Statistics() {
   }, []);
 
   return (
-    auth ? <h1>Здесь будет статистика</h1> : <h1>Для доступа к этой нужно вести пароль</h1>
+    auth ?
+      <Table striped bordered hover variant="dark">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Username</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td colSpan="2">Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </Table> : <h1>Для доступа к этой нужно вести пароль</h1>
   )
 }
