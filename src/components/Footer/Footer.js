a4f39af546936dcf24d9e6a56d35bc2672307b9c
Шашичев Кирилл <ekis@technodom.kz>
2021-10-18 12:19:22 +0600

    внести фикс

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,7 +6,7 @@ export default function Footer({ modelsData }) {
   return (
     <footer className="footer text-muted">
       <span className="footer__text">База данных поиска содержит <span className="text-warning">{modelsData.length}</span> {formatWord(modelsData.length, templateWordsNoun)}</span>
-      <span className="footer__text">Кирилл Шашичев <span class="text-warning">&diams;</span> ТД "Интант" &copy; {new Date().getFullYear()}</span>
+      <span className="footer__text">Кирилл Шашичев <span class="text-warning">&#10007;</span> ТД "Интант" &copy; {new Date().getFullYear()}</span>
     </footer>
   )
 }
