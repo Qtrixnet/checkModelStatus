fe0f2cbb1543a1868e9077bb3ba3dfd9cc3cda3d
Kirill Shashichev <67040346+Qtrixnet@users.noreply.github.com>
2021-10-20 00:23:50 +0600

    убрать градиент

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import { templateWordsNoun } from '../../utils/constants';
 
 export default function Footer({ modelsData }) {
   return (
-    <footer className="footer text-muted">
+    <footer className="footer">
       <span className="footer__text">База данных поиска содержит <span className="text-warning">{modelsData.length}</span> {formatWord(modelsData.length, templateWordsNoun)}</span>
       <span className="footer__text">Кирилл Шашичев <span className="text-warning">&#10007;</span> ТОО Торговый дом "Интант" &copy; {new Date().getFullYear()}</span>
     </footer>
